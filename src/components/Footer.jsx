3dce30fa92eb310fa685b9b001e62b552950ae3b
Knopka <knopkaivy@gmail.com>
2023-01-07 12:34:56 -0600

    add Footer component and styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      © 2035 by Emilia Ingram | Realtor. For demo purposes only. Design by
+      Wix.com
+    </div>
+  );
+};
+
+export default Footer;
