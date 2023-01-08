a58e4bf988c077f522241f5a0587160c71ff562f
Knopka <knopkaivy@gmail.com>
2023-01-08 07:42:23 -0600

    add ScheduleButton component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './styles/App.css';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import ScheduleButton from './components/ScheduleButton';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ScheduleButton />
       <Contact />
       <Footer />
     </div>
---
+++ src/components/ScheduleButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/ScheduleButton.css';
+
+const ScheduleButton = () => {
+  return <button className="ScheduleButton">Schedule a Tour</button>;
+};
+
+export default ScheduleButton;
