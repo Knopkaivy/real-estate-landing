dd61cf2ff6600ad3b4ef9165ed9db605242f95df
Knopka <knopkaivy@gmail.com>
2023-01-08 07:51:48 -0600

    add MainPage component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import ScheduleButton from './components/ScheduleButton';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <ScheduleButton />
+      <MainPage />
       <Contact />
       <Footer />
     </div>
