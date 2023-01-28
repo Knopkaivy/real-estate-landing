0cc45fe364be7963e96a42acbd7f1b8253a6ebf1
Knopka <knopkaivy@gmail.com>
2023-01-28 07:22:42 -0600

    add index route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import './styles/App.css';
+import { Routes, Route } from 'react-router-dom';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import ScheduleButton from './components/ScheduleButton';
 import MainPage from './components/MainPage';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <ScheduleButton />
-      <MainPage />
+      <Routes>
+        <Route index element={<MainPage />} />
+      </Routes>
       <Contact />
       <Footer />
     </div>
