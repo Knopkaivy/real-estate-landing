b5abc894989d62bed078c6e6162e93a6fc148649
Knopka <knopkaivy@gmail.com>
2023-02-08 08:22:09 -0600

    add schedule route and Link

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import ScheduleButton from './components/ScheduleButton';
+import ScheduleTour from './components/ScheduleTour';
 import MainPage from './components/MainPage';
 import './styles/App.css';
 import More from './components/More';
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/more" element={<More />} />
+        <Route path="/schedule" element={<ScheduleTour />} />
       </Routes>
       <Contact />
       <Footer />
--- src/components/ScheduleButton.jsx
+++ src/components/ScheduleButton.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/ScheduleButton.css';
 
 const ScheduleButton = () => {
-  return <button className="ScheduleButton">Schedule a Tour</button>;
+  return (
+    <button className="ScheduleButton">
+      <Link to="/schedule">Schedule a Tour</Link>
+    </button>
+  );
 };
 
 export default ScheduleButton;
