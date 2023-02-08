255b303c3f83b2910901462bd176de07a3a1c670
Knopka <knopkaivy@gmail.com>
2023-02-08 07:35:11 -0600

    add ImageOverlay to Gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ImageOverlay from './ImageOverlay';
 import Images from './Images';
 
 import '../styles/Gallery.css';
 
 const Gallery = () => {
+  const [imageOverlayOpen, setImageOverlayOpen] = useState(false);
+  const [openingImageIndex, setOpeningImageIndex] = useState();
+
+  let closeOverlay = () => setImageOverlayOpen(false);
+
+  let openOverlay = (e) => {
+    if (e.target.id) {
+      setOpeningImageIndex(e.target.id);
+      setImageOverlayOpen(true);
+    }
+  };
+
   const imageList = Images.slice(0, 5).map((image, index) => {
     return (
       <div
         key={index}
+        onClick={openOverlay}
         className={`Gallery__image ${index === 0 &&
           'Gallery__gridItem--small'} ${index === 4 &&
           'Gallery__gridItem--small'} ${index === 1 &&
@@ -21,6 +35,11 @@ const Gallery = () => {
   });
   return (
     <div id="gallery" className="Gallery">
+      <ImageOverlay
+        isOpen={imageOverlayOpen}
+        openingImageIndex={openingImageIndex}
+        closeOverlay={closeOverlay}
+      />
       <div className="Gallery__imageContainer">{imageList}</div>
     </div>
   );
