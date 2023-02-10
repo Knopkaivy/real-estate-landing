d4e664f23f28bd8994eaacb9a8160fe0344cb8f3
Knopka <knopkaivy@gmail.com>
2023-02-10 10:41:35 -0600

    add react-datepicker and styles

--- src/components/ScheduleTour.jsx
+++ src/components/ScheduleTour.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import { addDays, getDay, setHours, setMinutes, subDays } from 'date-fns';
 import { useNavigate } from 'react-router-dom';
 import { BsChevronLeft } from 'react-icons/bs';
 import '../styles/ScheduleTour.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const ScheduleTour = () => {
   const navigate = useNavigate();
+  const [startDate, setStartDate] = useState();
+  const isWeekday = (date) => {
+    const day = getDay(date);
+    return day !== 0 && day !== 6;
+  };
+  const filterPassedTime = (time) => {
+    const currentDate = new Date();
+    const selectedDate = new Date(time);
+
+    return currentDate.getTime() < selectedDate.getTime();
+  };
   return (
     <div className="ScheduleTour">
       <div className="ScheduleTour__container container">
@@ -29,7 +43,24 @@ const ScheduleTour = () => {
           <div className="ScheduleTour__dateAndTime">
             <h3 className="ScheduleTour__dtHeader">Select a Date and Time</h3>
             <div className="ScheduleTour__dtContainer">
-              <div className="ScheduleTour__date">Date</div>
+              <div className="ScheduleTour__date">
+                <DatePicker
+                  className="ScheduleTour__datePicker"
+                  selected={startDate}
+                  showTimeSelect
+                  minDate={subDays(new Date(), 0)}
+                  maxDate={addDays(new Date(), 30)}
+                  showDisabledMonthNavigation
+                  minTime={setHours(setMinutes(new Date(), 0), 9)}
+                  maxTime={setHours(setMinutes(new Date(), 0), 17)}
+                  dateFormat="MMMM d, yyyy h:mm aa"
+                  filterDate={isWeekday}
+                  filterTime={filterPassedTime}
+                  placeholderText="Select Date and Time"
+                  timeIntervals={60}
+                  onChange={(date) => setStartDate(date)}
+                />
+              </div>
               <div className="ScheduleTour__time">Time</div>
             </div>
           </div>
