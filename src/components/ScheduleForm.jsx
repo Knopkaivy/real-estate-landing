e57aff06364fffb86d5ae881df8cba832a758c59
Knopka <knopkaivy@gmail.com>
2023-02-10 13:22:09 -0600

    add ScheduleForm component

---
+++ src/components/ScheduleForm.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/ScheduleForm.css';
+
+const ScheduleForm = () => {
+  return <form className="ScheduleForm">Schedule Form goes here</form>;
+};
+
+export default ScheduleForm;
